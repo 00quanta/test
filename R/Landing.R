90f5a45a2dc0aafba508201e1bcc5329137703a1
00quanta <xiemaozhou@icloud.com>
2018-01-30 17:59:37 +0800

    Update Landing.R

--- R/Landing.R
+++ R/Landing.R
@@ -1,62 +1,44 @@
-# for (i in 1:2){
-A1 <- read.csv(file.choose(),header = TRUE)
+# total_1 <- read.csv(file.choose(),header = TRUE)
+# for (i in 1:6){
+#   A[i] <- read.csv(file.choose(),header = TRUE)
+#   total_1 <- rbind(total, A[i])
 # }
-total<-rbind(A1,A2)
 
-library('dplyr')
-# 筛选含有HDTC的数据
-Landing <- filter(total, 
-                  grepl('HDTC5', utm_media)
-                  )
-Landing2 <- group_by(Landing, 
-                     网站名称, 
-                     utm_source, 
-                     城市, 
-                     utm_media
-                     )
-Landing3 <- mutate(Landing2,
-                   素材 = substr(utm_media, 30, 35), # nchar在这种情况如何运用 nchar('HS-201711478_INT324_20071024_HDTC50')
-                   素材分类 = gsub('HDTC50', '科迈罗', 素材)
-                   )
-
-# 统计一周分城市总landing
-by_city <- summarise(Landing3,
-                     landing = sum(访问数, na.rm = TRUE)
-)
-final <- filter(by_city, landing >= 10)
-# 输出结果为.csv
-write.csv(final,file = "E:/OTV/xuefulan-Landing.csv")
-
-
-a <- c("foo_5", "bar_7")
-a <- gsub(".*_", "", a)
-# for (i in 1:2){
-A1 <- read.csv(file.choose(),header = TRUE)
-A2 <- read.csv(file.choose(),header = TRUE)
+# files <- dir("E:/OTV/Landing/别克昂科威-Landing/test")
+# for (i in files){
+#   tempcsv <- read.csv(i)
 # }
-total<-rbind(A1,A2)
 
+A1 <- read.csv(file.choose(),header = TRUE)
+total <- rbind(A1, A2, A3, A4, A5, A6, A7)
 library('dplyr')
-# 筛选含有HDTC的数据
-Landing <- filter(total, 
-                  grepl('HDTC5', utm_media)
-                  )
+library('tidyr')
+# 筛选含有HDTC的数据, 注意修改‘HDTC’所在字段
+
+Landing <- total %>%
+  filter(grepl('HDTC5', utm_campaign)
+         )
 Landing2 <- mutate(Landing,
-                   素材 = substr(utm_media, 30, 35), # ？nchar在这种情况如何运用 nchar('HS-201711478_INT324_20071024_HDTC50')
-                   素材分类 = gsub('HDTC50', '科迈罗', 素材),
-                   素材分类 = gsub('HDTC51', '科鲁兹', 素材分类),
-                   素材分类 = gsub('HDTC52', '探界者', 素材分类)
-                   )
+    素材 = substr(utm_campaign, nchar(as.character(Landing$utm_campaign))-5, nchar(as.character(Landing$utm_campaign))),
+    素材版本 = gsub('HDTC55', 'A', 素材),
+    素材版本 = gsub('HDTC56', 'B', 素材版本),
+    素材版本 = gsub('HDTC57', 'C', 素材版本)
+    )
+Landing3 <- unite(Landing2, "端口", 网站名称, utm_medium, sep = "_")
 
-by_city <- Landing2 %>%
-  group_by(网站名称, 
-           utm_source, 
-           城市, 
-           素材分类
+by_city <- Landing3 %>%
+  group_by(utm_source,
+           # utm_medium, 
+           端口, 
+           素材版本,
+           城市
            ) %>%
+  filter(素材版本 %in% c('A', 'B', 'C')) %>%
+  filter(端口 %in% c('别克官网-PC_PC', '别克官网-移动_APP')) %>%
   summarise(landing = sum(访问数, na.rm = TRUE)
-  ) %>%
-  filter(landing > 10) %>%
-  arrange(素材分类)
+  ) 
+# %>%
+#   filter(landing > 10) %>%
+  # arrange(素材版本)
 # 输出结果为.csv
-write.csv(by_city,file = "E:/OTV/xuefulan-Landing.csv")
+write.csv(by_city,file = "E:/OTV/angkewei-Landing.csv")
